672f6dc226bf1c8bc7148fadc2c946c06c4e0df2
Sean McHenry <sean@seanmchenry.com>
2015-03-26 09:52:42 -0400

    Updated favicons and fixed header link tags

--- header.php
+++ header.php
@@ -13,23 +13,23 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<!-- Favicons -->
-<link rel="apple-touch-icon-precomposed" href="<?php bloginfo( 'template_directory' ); ?>/assets/img/icons/favicon-152.png">
-<meta name="msapplication-TileColor" content="#333333">
-<meta name="msapplication-TileImage" content="<?php bloginfo( 'template_directory' ); ?>/assets/img/icons/favicon-144.png">
 
 <!-- Web fonts -->
 <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:400italic,300,600,400' rel='stylesheet' type='text/css'>
 
+<!-- Favicons -->
+<link rel="apple-touch-icon" href="<?php bloginfo( 'template_directory' ); ?>/assets/img/icons/apple-touch-icon.png">
+<link rel="icon" href="<?php bloginfo( 'template_directory' ); ?>/assets/img/icons/favicon.png">
+<meta name="msapplication-TileColor" content="#89A6B9">
+<meta name="msapplication-TileImage" content="<?php bloginfo( 'template_directory' ); ?>/assets/img/icons/win8-tile-icon.png">
+
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/favicon.ico">
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
 <?php wp_head(); ?>
